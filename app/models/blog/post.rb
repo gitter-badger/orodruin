fea056f7f23d9824e6d690708e1d1250587259d3
Łukasz Niemier <lukasz@niemier.pl>
2014-02-23 23:06:31 +0100

    Add slugs to blog posts

--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -1,6 +1,7 @@
 class Blog::Post
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Slug
 
   field :title, type: String
   field :content, type: String
@@ -8,10 +9,13 @@ class Blog::Post
 
   validates :title, presence: true, length: { maximum: 256 }
   validates :content, presence: true
+  validates :content_raw, presence: true
 
-  belongs_to :user
+  #belongs_to :user
 
   embeds_many :comments, class_name: 'Blog::Comment'
+
+  slug :title
 
   def parse_content
     self[:content] = ContentParser.new.parse(content_raw)
