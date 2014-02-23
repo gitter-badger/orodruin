b9f9b24d1b20a58e3975bed3c835cd41c9a0cb94
Łukasz Niemier <lukasz@niemier.pl>
2014-02-23 23:16:04 +0100

    User fabricator

---
+++ test/fabricators/user_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:user) do
+  first_name { Faker::Name.first_name }
+  last_name  { Faker::Name.last_name }
+
+  nickname { Faker::Internet.user_name }
+
+  email { Faker::Internet.email }
+end
