19d6f72bb84eb0125f7c1f3c3b8796a8baae00e3
Łukasz Niemier <lukasz@niemier.pl>
2014-02-24 17:31:36 +0100

    Enable Bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,5 +32,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = {}
+  config.action_mailer.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
+
+  config.after_initialize do
+    Bullet.enable       = true
+    Bullet.alert        = true
+    Bullet.rails_logger = true
+    Bullet.console      = true
+  end
 end
