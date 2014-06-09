039508840b6f9d29614179b08801e84604e84713
Łukasz Niemier <lukasz@niemier.pl>
2014-06-09 16:55:58 +0200

    Cleanup assets

--- app/assets/javascripts/components/nprogress.js
+++ app/assets/javascripts/components/nprogress.js
@@ -1,6 +1,8 @@
 //= require nprogress
 // jshint strict:false, undef:false
 
+NProgress.configure({ showSpinner: false });
+
 $(document).on('page:fetch',    function() { NProgress.start();  });
 $(document).on('page:change',   function() { NProgress.done();   });
 $(document).on('page:restore',  function() { NProgress.remove(); });
